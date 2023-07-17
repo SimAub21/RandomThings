61cd07e2cbe93db3b75cefe2fbd37755145c075c
Auburger Simon <simreiaub5@gmail.com>
2023-07-17 20:18:09 +0200

    improvements

--- Calculator/main.js
+++ Calculator/main.js
@@ -1,12 +1,35 @@
 function calculate() {
     console.log("calculate function called")
 
-    var number1 = parseInt(document.getElementById('number1').value);
-    var number2 = parseInt(document.getElementById('number2').value);
+    var number1 = parseFloat(document.getElementById('number1').value);
+    var number2 = parseFloat(document.getElementById('number2').value);
+    var operator = document.getElementById('operator').value
+    var result = 0
 
-    var result = number1 + number2
-    console.log(number1 + number2)
+    switch(operator)
+    {
+        case '+':
+            result = number1 + number2
+            break
+        case '-':
+            result = number1 - number2
+            break
+        case '*':
+            result = number1 * number2
+            break
+        case '/':
+            if(number2 == 0) {
+                console.log("Nicht durch Null teilen")
+                document.getElementById('result').innerText = "Nicht durch Null teilen"
+            }
+            else {
+                result = number1 / number2
+            }
+            break
+    }
 
-    document.getElementById('result').innerText = result
+    if(result != 0) {
+        document.getElementById('result').innerText = result
+    }
 
 }
